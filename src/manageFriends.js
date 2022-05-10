bd5a6ad2d9b46f7fa4953fc557b8bcfa8d477127
jacksongoldie <goldiejackson25@yahoo.com>
2022-05-10 08:56:30 -0500

    commit tree 97c8966396f068bdeeb7ebc7484cffe54378fbd6

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,13 @@
+const state = { friends: [] }
+
+
 export function manageFriends(state, action) {
-  // your code here
+  switch (action.type){
+    case "friends/add":
+      return { friends: [...state.friends, action.payload] };
+    case "friends/remove":
+      return { friends: state.friends.filter(x => x.id !== action.payload)};
+    default:
+      return state;
+  }
 }
